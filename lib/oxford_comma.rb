19c4ae0bb7079ca2566c19b339e358131661d3a3
itzcookiie <mayowaadeniyi@hotmail.com>
2018-10-29 00:57:25 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
-end
+def oxford_comma(arr)
+    if arr.length == 2
+        arr[-2] << " and "
+        arr.join
+    elsif arr.length == 1
+        arr.join
+    elsif arr.length > 2
+        arr[-1].prepend "and "
+        arr.join(", ")
+    end
+end 
